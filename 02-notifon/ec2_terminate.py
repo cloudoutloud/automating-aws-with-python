2d1fdc0820926545a4b5cd6d0638964a96d6f9fa
cloudoutloud <seanrigby26@gmail.com>
2019-05-19 17:49:04 +0100

    updated hash comment

--- 02-notifon/ec2_terminate.py
+++ 02-notifon/ec2_terminate.py
@@ -4,7 +4,7 @@ import boto3
 
 session = boto3.Session(profile_name='cr-labs-CF')
 ec2 = session.resource('ec2')
-ids = ['i-0b344280640ca12bc']
+ids = ['i-0b344280640ca12bc'] #Hard coded needs to be changed
 
 ec2.instances.filter(InstanceIds=ids).stop()
 print('Instance has been stopped')
